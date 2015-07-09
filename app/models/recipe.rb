a77fe3390ebc73d301c8b613efcba93f0cfcfe43
Steve Conklin <steve@conklinhouse.com>
2015-07-08 20:25:59 -0500

    Add pagination

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,7 +6,8 @@ class Recipe < ActiveRecord::Base
   validates :summary, presence: true, length: {minimum: 10, maximum: 150}
   validates :description, presence: true, length: {minimum: 20, maximum: 500}
   mount_uploader :picture, PictureUploader
-  validate :picture_size 
+  validate :picture_size
+  default_scope -> { order(updated_at: :desc) }
 
   def thumbs_up_total
     self.likes.where(like: true).size
